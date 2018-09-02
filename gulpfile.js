3b7a4b1648f0bde8ab404a9d0217681d1adbe0d3
sangumee <super2451894@gmail.com>
2018-09-02 13:54:33 +0900

    Gulp.js study
    Gulp.js study

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+ browserSync.init({
+     server: "./"
+ });
+ browserSync.stream();
+
+gulp.task('default', function () {
+    console.log('Hello Gulp!');
+});
+
+gulp.task('styles', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css'))
+});
