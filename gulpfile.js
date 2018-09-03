dd83eaf74b62d1d188dd307c14caa5ddced01db6
sangumee <super2451894@gmail.com>
2018-09-03 19:43:30 +0900

    GULP study
    GULP study

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
+/*eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-browserSync.init({
-    server: "./"
-});
-browserSync.stream(); // 
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function () {
-    console.log('Hello Gulp!');
+gulp.task('lint', function () {
+	return gulp.src(['js/**/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
 });
+
+gulp.task('default', ['styles', 'lint'], function () {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+
+	browserSync.init({
+		server: './'
+	});
+});
+
+
 
 gulp.task('styles', function () {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./css'))
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
+		browserSync.stream(); // 
 });
